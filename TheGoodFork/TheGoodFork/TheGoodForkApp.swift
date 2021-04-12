69c10bc70ded417b4aa6cdcbd3aa475a2d2654f0
Zaiden13 <louan.leplae@gmail.com>
2021-04-12 04:01:18 -0700

    initialisation du projet IOS

---
+++ TheGoodFork/TheGoodFork/TheGoodForkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheGoodForkApp.swift
+//  TheGoodFork
+//
+//  Created by admin on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TheGoodForkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
